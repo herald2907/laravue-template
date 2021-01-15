8992e59de10e0714c1107aab804f49a5de5fc940
Jyson <jbdelfin@curoteknika.com>
2021-01-15 12:44:28 +0800

    add login

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
@@ -13,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::get('/', [AuthController::class, 'index']);
 Route::get('/dashboard', [DashboardController::class, 'index']);
