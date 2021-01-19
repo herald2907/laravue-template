e256279e07f7699195f32447b51dfc6144c5b985
Herald Jay Lingat <heraldjay29@gmail.com>
2021-01-19 16:26:44 +0800

    Testing Laravel and Vue combined

---
+++ resources/js/router.js
@@ -0,0 +1,23 @@
+import { createWebHistory, createRouter } from "vue-router";
+import Home from "./vue/Home.vue";
+import About from "./vue/About.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/about",
+    name: "About",
+    component: About,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
---
+++ resources/js/vue/About.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>This is About page</div>
+</template>
+
+<script>
+export default {};
+</script>
