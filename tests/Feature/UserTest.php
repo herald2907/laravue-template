cf6afb6819c3da78adc4bf92e618cc200addd282
Jyson <jbdelfin@curoteknika.com>
2021-02-09 16:09:00 +0800

    update user creation in test

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -2,11 +2,11 @@
 
 namespace Tests\Feature;
 
+use App\Models\Role;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Storage;
-use Spatie\Permission\Models\Role;
 use Tests\TestCase;
 
 class UserTest extends TestCase
